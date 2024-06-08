67838abf0855b4e54b8122bee74432879229996d
Santiago Sanchez <ssanchez@kuenta.co>
2024-06-07 20:02:20 -0500

    add classes vehicle detect model

--- process/vehicle_detection/models/vehicle_detection.py
+++ process/vehicle_detection/models/vehicle_detection.py
@@ -1,4 +1,4 @@
 from typing import List
 
 vehicle_detect_model: str = 'process/vehicle_detection/models/vehicle_detection.pt'
-vehicle_detect_classes: List[str] = ['']
+vehicle_detect_classes: List[str] = ['bus', 'car', 'microbus', 'motorbike', 'pickup-van', 'truck']
